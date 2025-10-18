63fa8d5906e4cdc246c424863dec37854d9ecba1
NewJKH <newsun0805@gmail.com>
2025-10-18 13:02:17 +0900

    feat: cors Config 추가

---
+++ src/main/java/com/company/finsight/global/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.company.finsight.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	private final String webUrl = "http://localhost:3000";
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins(webUrl)
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+				.allowedHeaders("*")
+				.allowCredentials(true);
+	}
+}
