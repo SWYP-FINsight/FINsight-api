30965d99f9f0ecd8d01f2b326a0b70dc64355b97
NewJKH <newsun0805@gmail.com>
2025-10-18 11:01:54 +0900

    init

---
+++ src/main/java/com/company/finsight/FiNsightApplication.java
@@ -0,0 +1,13 @@
+package com.company.finsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiNsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiNsightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/finsight/FiNsightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.finsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiNsightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
