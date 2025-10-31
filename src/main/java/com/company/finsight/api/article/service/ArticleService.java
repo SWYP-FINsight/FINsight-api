7c900130b2e7bfa3ac90e95718aa60446262b767
Ljy981008 <wkdgorud75@naver.com>
2025-10-31 15:58:41 +0900

    fix : DB 초기화 및 스케줄링 재가동

--- src/main/java/com/company/finsight/api/article/service/ArticleService.java
+++ src/main/java/com/company/finsight/api/article/service/ArticleService.java
@@ -91,7 +91,7 @@ public class ArticleService {
         );
     }
 
-    //@Scheduled(fixedRate = 900000)
+    @Scheduled(fixedRate = 900000)
     public void test() {
         log.info("스케줄링 시작...");
         String category = "산업/기업"; // 현재 크롤링 중인 카테고리명
