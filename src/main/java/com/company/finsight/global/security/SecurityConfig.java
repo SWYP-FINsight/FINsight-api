0a7a900e581018559f55a044d107202cd8b302bb
Ljy981008 <wkdgorud75@naver.com>
2025-11-04 14:56:09 +0900

    refactor : 시큐리티 변경

--- src/main/java/com/company/finsight/global/security/SecurityConfig.java
+++ src/main/java/com/company/finsight/global/security/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
             )
 
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/finsight/**").permitAll()
+                .requestMatchers("/articles/**").permitAll()
                 .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                 .anyRequest().authenticated()
             )
