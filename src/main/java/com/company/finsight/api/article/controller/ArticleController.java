136246928608862bcad249107e0326ee27d4ae3c
Ljy981008 <wkdgorud75@naver.com>
2025-11-04 14:46:38 +0900

    refactor : API URL 변경

--- src/main/java/com/company/finsight/api/article/controller/ArticleController.java
+++ src/main/java/com/company/finsight/api/article/controller/ArticleController.java
@@ -14,7 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/finsight/articles")
+@RequestMapping("/articles")
 @RestController
 @RequiredArgsConstructor
 public class ArticleController {
