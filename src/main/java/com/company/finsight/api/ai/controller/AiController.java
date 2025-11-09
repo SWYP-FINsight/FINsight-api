6363438e2ca2aff0b1817fc26e879b335cdf9701
NewJKH <newsun0805@gmail.com>
2025-11-09 10:58:46 +0900

    refactor: ResponseEntity -> ApiResponse 공통응답으로 변경

--- src/main/java/com/company/finsight/api/ai/controller/AiController.java
+++ src/main/java/com/company/finsight/api/ai/controller/AiController.java
@@ -1,12 +1,15 @@
 package com.company.finsight.api.ai.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.company.finsight.api.ai.dto.SummarizeRequest;
+import com.company.finsight.api.ai.dto.SummarizeRequestDto;
+import com.company.finsight.api.ai.dto.SummarizeResponseDto;
 import com.company.finsight.api.ai.service.AiService;
+import com.company.finsight.global.response.ApiResponse;
 
 import lombok.AllArgsConstructor;
 
@@ -21,9 +24,11 @@ public class AiController {
 	 * }
 	 */
 	@PostMapping("/ai/summarize")
-	public ResponseEntity<String> summarize(@RequestBody SummarizeRequest request){
-		return ResponseEntity.ok(aiService.summarize(request.getArticleIds()));
+	public ResponseEntity<ApiResponse<SummarizeResponseDto>> summarize(@RequestBody SummarizeRequestDto request){
+		return ApiResponse.success(
+				HttpStatus.OK,
+				"선택된 기사들이 요약되었습니다.",
+				aiService.summarize(request)
+		);
 	}
-
-
 }
