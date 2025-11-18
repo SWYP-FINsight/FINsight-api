c58e1e0527563d61d1b71e173e3fe70950f320b0
NewJKH <newsun0805@gmail.com>
2025-11-18 11:30:33 +0900

    feat: 단일기사 API 추가

--- src/main/java/com/company/finsight/api/ai/service/AiService.java
+++ src/main/java/com/company/finsight/api/ai/service/AiService.java
@@ -1,6 +1,7 @@
 package com.company.finsight.api.ai.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,7 +9,10 @@ import org.springframework.transaction.annotation.Transactional;
 import com.company.finsight.api.ai.client.AIClient;
 import com.company.finsight.api.ai.dto.SummarizeRequestDto;
 import com.company.finsight.api.ai.dto.SummarizeResponseDto;
+import com.company.finsight.api.ai.dto.SummarizeSingleRequestDto;
 import com.company.finsight.api.article.service.ArticleService;
+import com.company.finsight.global.exception.business.article.ArticleErrorCode;
+import com.company.finsight.global.exception.business.article.ArticleException;
 
 import lombok.AllArgsConstructor;
 
@@ -23,6 +27,18 @@ public class AiService {
 	public SummarizeResponseDto summarize(SummarizeRequestDto requestDto) {
 		List<String> contents = articleService.findContentsByIds(requestDto.getArticleIds());
 		String summary = aiClient.summarize(contents);
+
+		return SummarizeResponseDto.toEntity(summary);
+	}
+
+	@Transactional(readOnly = true)
+	public SummarizeResponseDto summarize(SummarizeSingleRequestDto requestDto) {
+
+		List<String> contents = articleService.findContentsByIds(List.of(requestDto.getArticleId()));
+		Optional<String> contentOpt = contents.stream().findFirst();
+
+		String content = contentOpt.orElseThrow(()->new ArticleException(ArticleErrorCode.ARTICLE_NOT_FOUND));
+		String summary = aiClient.summarize(content);
 
 		return SummarizeResponseDto.toEntity(summary);
 	}
